0e8fc805f4efe9b67fb7f4b1bb97cb4655de2fed
KY-G14 <youngkevin7447@gmail.com>
2026-01-07 20:17:03 -0800

    Cleanup and organization for code and comments.

--- server.py
+++ server.py
@@ -11,6 +11,14 @@ import fastapi
 import uvicorn
 
 
+# This constant represents the port that the local server will run on.
+PORT = 8080
+
+# This constant represents the number of weeks in the future to 
+# consider for assignments with due dates.
+WEEKS_DELTA = 2
+
+
 app = fastapi.FastAPI()
 
 
@@ -21,6 +29,8 @@ def get_upcoming_assignments():
     the assignments as a list of dictionaries.
     '''
 
+    # 'TOKEN' is currently unused as the Canvas token is hardcoded into
+    # the canvas_assignment_api module.
     canvas_api = canvas_assignment_api.CanvasApiAssignments('TOKEN')
 
     assignments = canvas_api.get_all_assignments()
@@ -28,11 +38,11 @@ def get_upcoming_assignments():
     if not assignments:
         return
 
-    filtered_assignments = canvas_api.filter_assignments_due(assignments)
+    filtered_assignments = canvas_api.filter_assignments_due(assignments,
+                                                             WEEKS_DELTA)
 
     return filtered_assignments
 
 
 if __name__ == '__main__':
-    uvicorn.run("server:app", port = 8080, log_level = "info")
-
+    uvicorn.run("server:app", port = PORT, log_level = "info",)
