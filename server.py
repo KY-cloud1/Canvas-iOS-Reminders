46eef9b61f34c55ff1e03ee22e4bf1a5fbc07811
KY-G14 <youngkevin7447@gmail.com>
2026-01-07 17:53:19 -0800

    Updated to display upcoming Canvas assignments on local server.

--- server.py
+++ server.py
@@ -5,3 +5,34 @@ This module contains a FastAPI application that uses Uvicorn (ASGI)
 to send JSON data.
 '''
 
+
+import canvas_assignment_api
+import fastapi
+import uvicorn
+
+
+app = fastapi.FastAPI()
+
+
+@app.get('/assignments')
+def get_upcoming_assignments():
+    '''
+    Gets assignments from Canvas, filters into upcoming, and returns
+    the assignments as a list of dictionaries.
+    '''
+
+    canvas_api = canvas_assignment_api.CanvasApiAssignments('TOKEN')
+
+    assignments = canvas_api.get_all_assignments()
+
+    if not assignments:
+        return
+
+    filtered_assignments = canvas_api.filter_assignments_due(assignments)
+
+    return filtered_assignments
+
+
+if __name__ == '__main__':
+    uvicorn.run("server:app", port = 8080, log_level = "info")
+
